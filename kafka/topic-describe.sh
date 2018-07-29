6dd046f97844675963b03eafd1fd6dd0d35aeb48
Stephen Rufle <srufle@gmail.com>
2018-07-28 23:14:05 -0700

    Adding topic-describe.sh

---
+++ kafka/topic-describe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+KAFKA_IMAGE=exampleplatform/kafka:latest
+KAFKA_BIN="kafka/bin"
+ZOO_EXT="10.0.0.12:2181"
+TOPIC="$1"
+CONTAINER_NAME="kafka-worker"
+docker run --rm --name ${CONTAINER_NAME} ${KAFKA_IMAGE} "${KAFKA_BIN}/kafka-topics.sh" \
+           --describe --topic "${TOPIC}" --zookeeper "${ZOO_EXT}"
+
+echo "Done describing"
