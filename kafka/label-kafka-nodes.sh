8c68fbd0d132787e685d16c1c2b3147fc5db566e
Stephen Rufle <srufle@gmail.com>
2018-07-29 11:47:28 -0700

    Modified label nodes script to include more zookeeper and kafka nodes

--- kafka/label-kafka-nodes.sh
+++ kafka/label-kafka-nodes.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 docker node update --label-add zoo=1 dock-node-002
-docker node update --label-add kafka=1 dock-node-002
-docker node update --label-add kafka=2 dock-node-003
-docker node update --label-add kafka=3 dock-node-004
+docker node update --label-add zoo=2 dock-node-003
+docker node update --label-add zoo=3 dock-node-004
+docker node update --label-add kafka=1 dock-node-001
+docker node update --label-add kafka=2 dock-node-002
+docker node update --label-add kafka=3 dock-node-003
+docker node update --label-add kafka=4 dock-node-004
+docker node update --label-add kafka=5 dock-node-005
